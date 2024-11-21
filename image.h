d8b92cdd060acf68a0bdecf9096e7b05db17cbbb
SivarajAyyavoo <41547753+SivarajAyyavoo@users.noreply.github.com>
2024-11-21 07:15:41 +0530

    Use shared_ptr to avoid runtime error

--- image.h
+++ image.h
@@ -17,6 +17,14 @@ class Image {
     void SetPixel(uint16_t x, uint16_t y, uint8_t value) {
         pixels[x * m_columns + y] = value;
     }
+    bool ValidateImage() {
+        if (m_columns <= 1024 && m_rows <= 1024) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
     const uint16_t m_rows;
     const uint16_t m_columns;
     std::unique_ptr<uint8_t[]> pixels;  // max 1k x 1k image
